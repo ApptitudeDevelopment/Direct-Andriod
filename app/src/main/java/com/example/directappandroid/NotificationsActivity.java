8d7eb8a25ebb6ca874e9646a6d29166ff0aaf8f5
Chris <chris.xu.mps1@gmail.com>
2020-11-18 11:41:30 -0500

    Notification for Requests

--- app/src/main/java/com/example/directappandroid/NotificationsActivity.java
+++ app/src/main/java/com/example/directappandroid/NotificationsActivity.java
@@ -6,14 +6,34 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class NotificationsActivity extends AppCompatActivity {
     private RecyclerView notifications_list;
+    private DatabaseReference friendRequestReference, contactsRef, usersRef;
+
+    private FirebaseAuth mAuth;
+    private String currentUserId;
+    // private String listUserID = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +42,153 @@ public class NotificationsActivity extends AppCompatActivity {
 
         notifications_list = findViewById(R.id.notifications_list);
         notifications_list.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+
+        friendRequestReference = FirebaseDatabase.getInstance().getReference().child("Friend Requests");
+        contactsRef = FirebaseDatabase.getInstance().getReference().child("Contacts");
+        usersRef = FirebaseDatabase.getInstance().getReference().child("User");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<Contacts>()
+                .setQuery(friendRequestReference.child(currentUserId), Contacts.class).build();
+
+        FirebaseRecyclerAdapter<Contacts, NotificationsViewHolder> firebaseRecyclerAdapter
+                = new FirebaseRecyclerAdapter<Contacts, NotificationsViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final NotificationsViewHolder holder, int i, @NonNull Contacts contacts) {
+                holder.acceptBtn.setVisibility(View.VISIBLE);
+                holder.cancelBtn.setVisibility(View.VISIBLE);
+
+                final String listUserID = getRef(i).getKey();
+                DatabaseReference requestTypeRef = getRef(i).child("request_type").getRef();
+                requestTypeRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists())
+                        {
+                            String type = dataSnapshot.getValue().toString();
+
+                            if (type.equals("received"))
+                            {
+                                holder.cardView.setVisibility(View.VISIBLE);
+                                usersRef.child(listUserID).addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        if (dataSnapshot.hasChild("image"))
+                                        {
+                                            final String imageStr = dataSnapshot.child("image").getValue().toString();
+                                            Picasso.get().load(imageStr).into(holder.profileImageView);
+                                        }
+
+                                        final String nameStr = dataSnapshot.child("name").getValue().toString();
+                                        holder.userNameTxt.setText(nameStr);
+
+                                        holder.acceptBtn.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                contactsRef.child(currentUserId).child(listUserID).child("Contact").setValue("Saved")
+                                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if (task.isSuccessful())
+                                                                {
+                                                                    contactsRef.child(listUserID).child(currentUserId).child("Contact").setValue("Saved")
+                                                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                                @Override
+                                                                                public void onComplete(@NonNull Task<Void> task) {
+                                                                                    if (task.isSuccessful())
+                                                                                    {
+                                                                                        friendRequestReference.child(currentUserId).child(listUserID).removeValue()
+                                                                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                                                    @Override
+                                                                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                                                                        if (task.isSuccessful())
+                                                                                                        {
+                                                                                                            friendRequestReference.child(listUserID).child(currentUserId).removeValue()
+                                                                                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                                                                        @Override
+                                                                                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                                                                                            if (task.isSuccessful())
+                                                                                                                            {
+                                                                                                                                Toast.makeText(NotificationsActivity.this, "New Contact Saved.", Toast.LENGTH_SHORT).show();
+                                                                                                                            }
+                                                                                                                        }
+                                                                                                                    });
+                                                                                                        }
+                                                                                                    }
+                                                                                                });
+                                                                                    }
+                                                                                }
+                                                                            });
+                                                                }
+                                                            }
+                                                        });
+                                            }
+                                        });
+
+                                        holder.cancelBtn.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                friendRequestReference.child(currentUserId).child(listUserID).removeValue()
+                                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if (task.isSuccessful())
+                                                                {
+                                                                    friendRequestReference.child(currentUserId).child(listUserID).removeValue()
+                                                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                                @Override
+                                                                                public void onComplete(@NonNull Task<Void> task) {
+                                                                                    if (task.isSuccessful())
+                                                                                    {
+                                                                                        Toast.makeText(NotificationsActivity.this, "Request Cancelled", Toast.LENGTH_SHORT).show();
+                                                                                    }
+                                                                                }
+                                                                            });
+                                                                }
+                                                            }
+                                                        });
+                                            }
+                                        });
+                                    }
+
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
+                            else
+                            {
+                                holder.cardView.setVisibility(View.GONE);
+                                // Only display request that are sent to the user. For future,
+                                // maybe implement a list of all user's sent requests.
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public NotificationsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.find_friend_design, parent, false);
+                NotificationsViewHolder viewHolder = new NotificationsViewHolder(view);
+                return viewHolder;
+            }
+        };
+        notifications_list.setAdapter(firebaseRecyclerAdapter);
+        firebaseRecyclerAdapter.startListening();
     }
 
     public static class NotificationsViewHolder extends RecyclerView.ViewHolder {
@@ -41,4 +208,9 @@ public class NotificationsActivity extends AppCompatActivity {
             cardView = itemView.findViewById(R.id.card_view);
         }
     }
+
+//    private void AcceptFriendRequest() {
+//    }
+//    private void CancelFriendRequest() {
+//    }
 }
